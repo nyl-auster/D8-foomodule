5482167ea3afe287b8febdd971e8f948b929e324
Yann <yann.boisselier@gmail.com>
2013-11-08 18:26:48 +0100

    initial commit

---
+++ lib/Drupal/foomodule/Controller/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\foomodule\Controller;
+
+class HelloController extends \Drupal\Core\Controller\ControllerBase {
+
+  function content() {
+    return 'Hello world !';
+  }
+
+}
+
