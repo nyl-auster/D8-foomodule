62b77e4d97a79e8805d9d58ade7e336b8548ae65
Yann <yann.boisselier@gmail.com>
2013-11-09 15:10:13 +0100

    put settings in config folder or it cannot be loaded

--- lib/Drupal/foomodule/controller.php
+++ lib/Drupal/foomodule/controller.php
@@ -1,10 +1,31 @@
 <?php
+/**
+ * We could have put our controller in a "Controller" folder using
+ * Drupal\foomodule\Controller namespace instead of simplu Drupal\foomodule
+ *
+ * PSR-0 autoloader is used *inside* lib directory of each module, that's
+ * why we need for now to create this verbose directory structure in our module,
+ * or our class won't be autoloaded.
+ */
 namespace Drupal\foomodule;
 
-class controller {
+// use drupal ControllerBase, it provides us some shortcuts methods to
+// get our config, translate strings etc...
+// This is a class we may extends to our own class to provides us
+// usefull methods available in all our controllers for a specific project
+use Drupal\Core\Controller\ControllerBase;
 
-  function helloWorld() {
-    return 'Hello World';
+/**
+ * Extends Drupal ControllerBase, this is not required, we can also
+ * access config with \Drupal::config('foomodule.settings')
+ */
+class controller extends ControllerBase {
+
+  function hello() {
+    // config property is provided by ControllerBase class
+    // Get our module settings
+    $name = $this->config('foomodule.settings')->get('name');
+    return "Hello $name";
   }
 
 }
